21842f08dc075fbaf6462811e3cb04449ea239e0
ninajacques <nina.jacques@nextt.com.br>
2023-05-02 15:30:19 -0300

    add gtm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: process.env.GTM_ID
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const root = createRoot(document.querySelector("#root"));
 
