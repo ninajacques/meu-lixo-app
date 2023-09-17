88bac3da0f7823de98c2c4fba7c6de195549eb1c
ninajacques <nina.jacques@nextt.com.br>
2023-09-17 20:43:47 -0300

    remove Link import Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as C from "./styles";
 import IconButton from "../IconButton";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { signoutUser } from "../../firebase";
 
 const Header = () => {
